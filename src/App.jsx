b94ee4c7144a2eee9cd19f38a642341fe199730d
mithun6623 <enugulamithun1@gmail.com>
2026-02-11 12:05:14 +0530

    fix: updated import paths for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import About from './pages/About/About'
-import Home from './pages/Home/Home'
-import Contact from './pages/Contact/Contact'
-import Navbar from './Components/Navbar/Navbar'
-import Recipes from './Components/Recipes/Recipes'
-import SingleRecipe from './Components/SingleRecipe/SingleRecipe'
+import About from './pages/About/About.jsx'
+import Home from './pages/Home/Home.jsx'
+import Contact from './pages/Contact/Contact.jsx'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Recipes from './Components/Recipes/Recipes.jsx'
+import SingleRecipe from './Components/SingleRecipe/SingleRecipe.jsx'
 
 function App() {
   return (
